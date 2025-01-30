499c95e3d77037412dffdbc11040357ab451fe05
unknown <joaovictor.ricci1999@gmail.com>
2025-01-30 14:27:18 -0300

    feat: lógica JS para gerar as cartas aleatoriamente

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -18,11 +18,83 @@ const state = {
     }
 };
 
-//
-function init(){
+const playerSides = { 
+    player1 : "player-cards",
+    computer: "computer-cards",
+}
+const pathImages = "./src/assets/icons/";
+const cardData = [
+    {
+        id: 0,
+        name: "Blue Eyes White Dragon",
+        type: "Paper",
+        img: `${pathImages}dragon.png`,
+        WinOf: [1],
+        LoseO: [2],
+    },
+    {
+        id: 1,
+        name: "Dark Magician",
+        type: "Rock",
+        img: `${pathImages}magician.png`,
+        WinOf: [2],
+        LoseO: [0],
+    },
+    {
+        id: 2,
+        name: "Exodia",
+        type: "Scissors",
+        img: `${pathImages}exodia.png`,
+        WinOf: [0],
+        LoseO: [1],
+    },
+];
+async function createCardImage(IdCard, fieldSide) {
+    const cardImage = document.createElement("img");
+    cardImage.setAttribute("height", "100px");
+    cardImage.setAttribute("src", "./src/assets/icons/card-back.png");
+    cardImage.setAttribute("data-id", IdCard);
+    cardImage.classList.add("card");
 
+    if(fieldSide === playerSides.player1){
+        cardImage.addEventListener("mouseover", () => {
+            drawSelectCard(IdCard);
+        })
+        cardImage.addEventListener("click", () => {
+            setCardsField(cardImage.getAttribute("data-id"));
+            
+        })
+    }
+
+    
+
+    return cardImage;
+}
+async function getRandomCardId(){
+    const randomIndex = Math.floor(Math.random() * cardData.length);
+    return cardData[randomIndex].id;
+}
+
+async function drawSelectCard(index) {
+    state.cardSprites.avatar.src = cardData[index].img;
+    state.cardSprites.name.innerText = cardData[index].name;   
+    state.cardSprites.type.innerText = "Attribute: " + cardData[index].type;
+    
+}
+async function drawCards(cardNumbers, fieldSide){
+    for (let i = 0 ; i < cardNumbers; i ++){
+        const randomIdCard = await getRandomCardId();
+        const cardImage = await createCardImage(randomIdCard, fieldSide);
+
+        document.getElementById(fieldSide).appendChild(cardImage);
+
+    }
+}
+function init(){
+    drawCards(5, playerSides.player1);
+    drawCards(5, playerSides.computer);
 }
 
 init();
 
-//continuar no enumeração de cartas
+//continuar com a feat de setcards
